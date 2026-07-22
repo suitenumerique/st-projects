c988b78f97b740bb824dd98f2938e3e3f2b67e64
juliendemoutiez <julien.demoutiez@gmail.com>
2026-07-22 12:19:04 +0200

    🐛(dev) defer ResizeObserver callbacks to avoid benign dev-overlay error

--- client/src/index.js
+++ client/src/index.js
@@ -7,5 +7,19 @@ import Root from './components/Root';
 
 import './i18n';
 
+// Defers ResizeObserver callbacks to the next frame to avoid the benign
+// "ResizeObserver loop completed with undelivered notifications" browser
+// error, which webpack-dev-server's overlay otherwise reports as a crash.
+if (window.ResizeObserver) {
+  const OriginalResizeObserver = window.ResizeObserver;
+  window.ResizeObserver = class extends OriginalResizeObserver {
+    constructor(callback) {
+      super((entries, observer) => {
+        window.requestAnimationFrame(() => callback(entries, observer));
+      });
+    }
+  };
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(React.createElement(Root, { store, history }));
